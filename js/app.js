f80b0b531e3c05cfe16536d66d6c4b10dc65654c
minus78 <minus78dev@gmail.com>
2015-10-03 19:13:26 +0200

    ajout des balises code,pre dans le CSS et du Panel
    ajout des balises code,pre dans le CSS et du Panel
    ajout d'un footer
    ajout de hightlight
    modification des margins et padding

--- js/app.js
+++ js/app.js
@@ -3,4 +3,6 @@ jQuery(document).ready(function($)
         $('div.flash .right a').click(function(e){
             $(this).parent().parent().hide(100);
         });
+
+        hljs.initHighlightingOnLoad();
 });
