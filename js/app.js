8f475ae2141d04352e48257c5bddf1f5a03fcc0d
minus78 <minus78dev@gmail.com>
2015-10-04 03:48:59 +0200

    correcction bug
    correction bug

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 jQuery(document).ready(function($)
 {
-        $('div.flash .right a').click(function(e){
-            $(this).parent().parent().hide(100);
-        });
+    $('div.flash .right a').click(function(e){
+        $(this).parent().parent().hide(100);
+    });
 
-        hljs.initHighlightingOnLoad();
+    hljs.initHighlightingOnLoad();
+
+    if(!Modernizr.svg) {
+        $('img[src*="svg"]').attr('src', function() {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
 });
